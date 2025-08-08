e7dc6370724fbe49fb915d172cb725cc8766c044
Dankan26 <156017660+Dankan26@users.noreply.github.com>
2025-08-08 11:32:24 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Bruna Lavalle - Psicóloga',
+  description: 'Psicóloga especializada em saúde emocional, autoestima e relacionamentos.',
   generator: 'v0.dev',
 }
 
@@ -15,17 +15,59 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <head>
+        <link rel="icon" href="/favicon.ico" />
+
+        {/* Fonte do V0 */}
         <style>{`
-html {
-  font-family: ${GeistSans.style.fontFamily};
-  --font-sans: ${GeistSans.variable};
-  --font-mono: ${GeistMono.variable};
-}
+          html {
+            font-family: ${GeistSans.style.fontFamily};
+            --font-sans: ${GeistSans.variable};
+            --font-mono: ${GeistMono.variable};
+          }
         `}</style>
+        
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=GTM-WGSB4TNQ';f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WGSB4TNQ');
+            `,
+          }}
+        />
+
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KJKS92D4HE"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-KJKS92D4HE');
+            `,
+          }}
+        />
       </head>
-      <body>{children}</body>
+
+      <body>
+        {/* GTM fallback para usuários sem JS */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WGSB4TNQ"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+
+        {children}
+      </body>
     </html>
   )
 }
